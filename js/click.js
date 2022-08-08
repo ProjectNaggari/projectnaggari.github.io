28ce82ea6030fa9f8360558f803d31a27c83ef31
Mestyan <Mestyan@gmail.com>
2022-08-07 20:58:20 -0400

    Update click.js

--- js/click.js
+++ js/click.js
@@ -4,20 +4,20 @@ const element = document.querySelectorAll("[xml:lang=ar-Latn-x-lc]");
 element.addEventListener("click", myFunction);
 
 
-
-function myFunction() {
+//
+// function myFunction() {
   
-  document.querySelectorAll("[xml:lang=ar-Latn-x-lc]").click();
+  //document.querySelectorAll("[xml:lang=ar-Latn-x-lc]").click();
 
-  console.log(div.outerHTML);
+  //console.log(div.outerHTML);
 
-  div.classList.toggle("visible");
-
-}
+  //div.classList.toggle("visible");}
 
 
 
 
+
+//
 
 
 
@@ -25,36 +25,37 @@ function myFunction() {
 
 //
 
-const div = document.createElement('div');
-div.className = 'foo';
+//const div = document.createElement('div');
+//div.className = 'foo';
 
 // our starting state: <div class="foo"></div>
-console.log(div.outerHTML);
+c//onsole.log(div.outerHTML);
 
 // use the classList API to remove and add classes
-div.classList.remove("foo");
-div.classList.add("anotherclass");
+//div.classList.remove("foo");
+//div.classList.add("anotherclass");
 
 // <div class="anotherclass"></div>
-console.log(div.outerHTML);
+//console.log(div.outerHTML);
 
 // if visible is set remove it, otherwise add it
-div.classList.toggle("visible");
+//div.classList.toggle("visible");
 
 // add/remove visible, depending on test conditional, i less than 10
-div.classList.toggle("visible", i < 10 );
+//div.classList.toggle("visible", i < 10 );
 
-console.log(div.classList.contains("foo"));
+//console.log(div.classList.contains("foo"));
 
 // add or remove multiple classes
-div.classList.add("foo", "bar", "baz");
-div.classList.remove("foo", "bar", "baz");
+//div.classList.add("foo", "bar", "baz");
+//div.classList.remove("foo", "bar", "baz");
 
 // add or remove multiple classes using spread syntax
-const cls = ["foo", "bar"];
-div.classList.add(...cls);
-div.classList.remove(...cls);
+//const cls = ["foo", "bar"];
+//div.classList.add(...cls);
+//div.classList.remove(...cls);
 
 // replace class "foo" with class "bar"
-div.classList.replace("foo", "bar");
+//div.classList.replace("foo", "bar");
 //
+
